e7289372cb48815b8aed27baceb80152ad4f8c44
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-05-11 13:49:43 +0700

    remove page repo

--- src/lib/repositories/portal/mix-page-repository.ts
+++
@@ -1,9 +0,0 @@
-import { MixModelType } from '../../enums/mix-enums';
-import { MixRestPortalRepository } from '../../infrastructure/base/mix-rest-portal-repository';
-import { MixPostPortalModel } from '../../models/portal/mix-post-portal-model';
-
-export class PageService extends MixRestPortalRepository<MixPostPortalModel> {
-  constructor() {
-    super(MixModelType.Page);
-  }
-}
